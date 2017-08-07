b3bb1425b1bf2f93626fb56bde4828583ff244be
rijurashmi <30655137+rijurashmi@users.noreply.github.com>
2017-08-07 14:47:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@ console.log('Loaded!');
 
 // change the text of the main
 
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
+//var element = document.getElementById('main-text');
+//element.innerHTML = 'New Value';
 
 var img = document.getElementById('madi');
 var marginLeft =0;
