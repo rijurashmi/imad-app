fcecb634ab8e9ead9292b2ce2830999afc1cc122
rijurashmi <30655137+rijurashmi@users.noreply.github.com>
2017-08-07 14:13:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,16 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 var img = document.getElementById('madi');
+var marginLeft =0;
+function moveRight(){
+    marginLeft = marginLeft + 19;
+    img.style.marginLeft = marginLeft + 'px';
+}
 
+// 
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    
+  var interval = setInterval(moveRight,100);
+    
+//    img.style.marginLeft = '100px';
 };
