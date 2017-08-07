ad74ca414efc46c2cc588db81aec6be8c7bb4617
rijurashmi <30655137+rijurashmi@users.noreply.github.com>
2017-08-07 11:12:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,8 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
+var img = document.getElementById('madi');
+
+img.onClick = function () {
+    img.style.marginLeft = '100px';
+};
