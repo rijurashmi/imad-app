c02ed6c8ee13fb19d8bf96ec29fdf79960469a76
rijurashmi <30655137+rijurashmi@users.noreply.github.com>
2017-08-08 13:23:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,11 +58,11 @@ submit.onclick = function(){
     
     
     // Capture the list of names and render it as the list
-    var names = ['name1','name2','name3'];
+    var names = ['name1','name2','name3','name4'];
     var list ='';
     for(var i=0; i < names.length; i++){
         list += '<li>' + names[i] + '</li>';
     }
-    var ul = document.getElementById("");
-    
+    var ul = document.getElementById("nameList");
+    ul.innerList = list;
 };
