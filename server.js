9b547993a5fe1e485f7af04800dd53d4c4837cd9
rijurashmi <30655137+rijurashmi@users.noreply.github.com>
2017-08-09 11:35:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,12 +110,12 @@ app.get('/submit-name', function (req,res) { // URL : /submit-name?name=xxxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req,res){
+/*app.get('/:articleName', function(req,res){
    // articleName == article-one
    // articles[articleName] == {} content object for article one
    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName])); 
-});
+});*/
 
 
 app.get('/ui/style.css', function (req, res) {
