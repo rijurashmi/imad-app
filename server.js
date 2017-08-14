ca3bafe928cd63d5936d1e0f57af71b3393c4963
rijurashmi <30655137+rijurashmi@users.noreply.github.com>
2017-08-14 20:14:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Pool = require('pg').Pool;
 var config = {
     user: 'rijurashmi',
     database: 'rijurashmi',
-    host: 'db.imad.hosura-app.io',
+    host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
 };
