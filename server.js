a03186d02f7bbebb14ff34acbb6cbcc84ae44bfd
rijurashmi <30655137+rijurashmi@users.noreply.github.com>
2017-08-14 11:05:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var config = {
     user: 'rijurashmi',
     database: 'rijurashmi',
     host: 'db.imad.hosura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    port: '5432'
 };
 
 var app = express();
