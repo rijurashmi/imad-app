62bbe19ffd6d59adf0aa5f52e804846da7e483c7
rijurashmi <30655137+rijurashmi@users.noreply.github.com>
2017-08-16 10:08:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.get('/submit-name', function (req,res) { // URL : /submit-name?name=xxxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('articles/:articleName', function(req,res){
+app.get('/articles/:articleName', function(req,res){
    // articleName == article-one
    // articles[articleName] == {} content object for article one
    
