0de55caac2bd189c500bd59ecbef518324fab893
rijurashmi <30655137+rijurashmi@users.noreply.github.com>
2017-08-16 12:46:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,6 +148,7 @@ app.get('/articles/:articleName', function(req,res){
        }
        else{
            if(result.rows.length === 0){
+               res.send('test');
                res.status(404).send(err.toString());
            }
            else{
