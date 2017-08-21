698e0e803fe7a0f30b64cedf52a0cde5221b9d2e
rijurashmi <30655137+rijurashmi@users.noreply.github.com>
2017-08-21 13:35:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ function hash(input, salt){
     
     // How do we create the hash
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return ['pbksf', '10000', salt, hashed.toString('hex')].join('$');
+    return ['pbkdf2', '10000', salt, hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function(req,res) {
