99d397914d430e835d6cac566c5648f7a5455567
rijurashmi <30655137+rijurashmi@users.noreply.github.com>
2017-08-22 14:02:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,34 +20,8 @@ img.onclick = function () {
 //    img.style.marginLeft = '100px'; */
 
 
-// Counter Code
 
-var button = document.getElementById("counter");
-var counter = 0;
-button.onclick = function(){
-    
-    // Create a request object
-    var request = new XMLHttpRequest();
-    
-    // Capture the response and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            // Take some action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById("count");
-                span.innerHTML = counter.toString();
-            }
-        // Not yet done
-        }
-        
-    };
-    
-    request.open('GET','http://rijurashmi.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-
-// Submit name
+// Submit username/password to login
 
 var submit = document.getElementById("submit_btn");
 submit.onclick = function(){
@@ -60,23 +34,23 @@ submit.onclick = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             // Take some action
             if(request.status === 200){
-                // Capture the list of names and render it as the list
-                var names = request.responseText;
-                names= JSON.parse(names);
-                var list ='';
-                for(var i=0; i < names.length; i++){
-                    list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById("nameList");
-                ul.innerHTML = list;     
+               console.log('User logged in.');
+               alert('Logged in successfully');
             }
-        // Not yet done
+            else if(request.status === 403){
+                alert('username/password is incorrect');
+            }
+            else if(request.status === 500){
+                alert('Something went wrong on the server');
+            }
         }
     }
     
-    var nameInput = document.getElementById("name");
-    var name = nameInput.value;
-
-    request.open('GET','http://rijurashmi.imad.hasura-app.io/submit-name?name=' + name,true);
-    request.send(null);
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://rijurashmi.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username:username, password:password}));
 };
